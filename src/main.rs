727e34b3a864c3de05ee916a6a8dd09eed0a9303
Avery Wagar <ajmw.subs@gmail.com>
2018-06-01 11:33:07 -0700

    Added ans

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate rand;
 
-use rand::prelude::*;
-
 use std::env;
 use rand::prelude::*;
 use std::f64::consts::PI;
 use std::io::{stdin, stdout, Write};
 use std::process::Command;
+
+macro_rules! block {
+    ($xs:block) => {
+        loop { let _ = $xs; break; }
+    };
+}
+
+static HELP: &str = "Welcome to calc!\n================\n\nUsage:\n\n 'calc'  opens an interactive session\n 'calc help' opens this help menu\n 'calc <expression>'  solves expression and quits\n\nOperators:\n\n ^  Raise to power\n %  Modulus\n +  Addition\n -  Subtraction\n *  Mutiplication\n /  Division\n\nEquations:\n\n PI  first 14 digits of Π\n ans  previous answer\n\nCommands:\n\n help  opens this help menu\n clear  clear the screen\n quit  close calc";
 
 fn main() {
 
@@ -22,7 +28,7 @@ fn main() {
     /* START Argument handling */
 
     if command == "help" {
-        help();
+        println!("{}", HELP);
     } else if *&args.len() as i32 > 2 {
         // Create new string
         let mut stringer: String = String::new();
@@ -48,20 +54,21 @@ fn main() {
         // Print hint
         hint();
         // Start listner
-        run();
+        run(0.0);
     }
 
     /* END Argument handling */
 }
 
-fn calc(calc_arr: Vec<&str>, index: usize, mut val: f64, first: bool) -> f64 {
+fn calc(calc_arr: Vec<&str>, index: usize, mut val: f64, first: bool, ans: f64) -> f64 {
+
     if index != 0 || index != calc_arr.len() {
         if first {
-            val = handle_equ(calc_arr[0]);
+            val = handle_equ(calc_arr[0], ans);
         }
         let next_idx = (index + calc_arr.len() + 1) % calc_arr.len();
 
-        let right_hand = handle_equ(calc_arr[next_idx]);
+        let right_hand = handle_equ(calc_arr[next_idx], ans);
 
         if calc_arr[index].contains("/") {
             val /= right_hand;
@@ -79,23 +86,23 @@ fn calc(calc_arr: Vec<&str>, index: usize, mut val: f64, first: bool) -> f64 {
         if index == calc_arr.len() - 1 {
             return val;
         } else {
-            return calc(calc_arr, index + 1, val, false);
+            return calc(calc_arr, index + 1, val, false, ans);
         }
     }
-    return calc(calc_arr, index + 1, val, false);
+    return calc(calc_arr, index + 1, val, false, ans);
 }
 
-fn handle_command (string: &str) {
+fn handle_command (string: &str, ans: f64)  -> String {
     match string {
-        "quit"  => exit(0),
-        "clear" => clear(),
-        "help"  => help(),
-        _ => parse(string.to_string())
+        "quit"  => return exit(0),
+        "clear" => return clear(),
+        "help"  => return "\n".to_string(),
+        _ => return parse(string.to_string(), ans)
     }
 }
 
 
-fn handle_equ(string: &str) -> f64 {
+fn handle_equ(string: &str, ans: f64) -> f64 {
     match string {
         "+" => return 0.0,
         "-" => return 0.0,
@@ -104,11 +111,12 @@ fn handle_equ(string: &str) -> f64 {
         "/" => return 0.0,
         "%" => return 0.0,
         "pi" => return PI,
+        "ans" => return ans,
         _ => return string.parse::<f64>().unwrap(),
     }
 }
 
-fn run() {
+fn run(mut ans: f64) {
     let mut s = String::new();
     let _ = stdout().flush();
     stdin()
@@ -121,13 +129,35 @@ fn run() {
         s.pop();
     }
 
+    let string_help = HELP.to_string();
     // handle commands
-    handle_command(&*s);
+    let res = handle_command(&*s, ans);
+    let print: bool;
+    match &*res {
+        // Break
+        "Cleared!" => block!({ break; }),
+        // Break
+        "Goodbye!" => block!({ break; }),
+        // Break
+        "Please enter a valid expression." => block!({ break; }),
+        "\n" => println!("{}", HELP),
+        _ => ans = res.parse::<f64>().unwrap()
+
+    }
+
+    // Print res
+    if &res != "Cleared!"{
+        println!("{}", res);
+    }
     // recurse
-    run();
+
+    if &*res != "Goodbye" {
+        run(ans);
+    }
 }
 
-fn parse(s: String){
+
+fn parse(s: String, mut ans: f64) -> String {
 
     // New vector of splitted strings for command
     let calc_arr: Vec<&str>;
@@ -141,14 +171,12 @@ fn parse(s: String){
     if calc_arr.len() > 1 {
         let index = 1;
 
-        let res = calc(calc_arr, index, 0.0, true);
+        let res = calc(calc_arr, index, 0.0, true, ans);
 
-        println!("{}", res);
+        return res.to_string();
     } else {
-        println!("Please enter a valid expression.");
+        return "Please enter a valid expression.".to_string();
     }
-
-    run();
 }
 
 fn run_once(com: String) {
@@ -164,34 +192,30 @@ fn run_once(com: String) {
         let index = 1;
 
         // Calculate
-        let res = calc(calc_arr, index, 0.0, true);
+        let res = calc(calc_arr, index, 0.0, true, 0.0);
 
         println!("{}", res);
     } else {
-        println!("Please enter a valid expression.");
+        println!("{}", calc_arr[0]);
     }
 }
 
-fn clear() {
+fn clear() -> String {
     // Clear screen and start
     let output = Command::new("clear")
         .output()
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
     // Print out clear terminal
-    print!("{}", String::from_utf8_lossy(&output.stdout));
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+
+    return "Cleared!".to_string();
 }
 
-fn exit(code: i32){
+fn exit(code: i32) -> String {
     clear();
-    println!("Goodbye!");
     std::process::exit(code);
-}
-
-fn help(){
-    let help = "Welcome to calc!\n================\n\nUsage:\n\n 'calc'  opens an interactive session\n 'calc help' opens this help menu\n 'calc <expression>'  solves expression and quits\n\nOperators:\n\n ^  Raise to power\n %  Modulus\n +  Addition\n -  Subtraction\n *  Mutiplication\n /  Division\n\nEquations:\n\n PI  first 14 digits of Π\n\nCommands:\n\n help  opens this help menu\n clear  clear the screen\n quit  close calc";
-    // Print help
-    println!("{}", help);
+    return "Goodbye!".to_string();
 }
 
 fn hint(){
---
+++ target/rls/debug/libcalc-d562646dba4b0c36.d
@@ -0,0 +1,1 @@
+/home/ajmwagar/usr/dev/git/calc/target/rls/debug/libcalc-d562646dba4b0c36.rmeta: /home/ajmwagar/usr/dev/git/calc/src/main.rs
